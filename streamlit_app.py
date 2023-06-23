5c2a79a92f5662da8e6c7a22b140a5c1544feca6
shrutireddy1229 <43026623+shrutireddy1229@users.noreply.github.com>
2023-06-23 14:15:50 -0700

    Adding pandas to streamlit_app.py
    Added pandas, imported a CSV file from S3 and created a data frame and displayed it in Streamlit app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title('My parents new healthy dinner')
 
 streamlit.header('Breakfast Menu')
@@ -8,3 +9,6 @@ streamlit.text('🐔Hard-Boiled free-range eggs')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
