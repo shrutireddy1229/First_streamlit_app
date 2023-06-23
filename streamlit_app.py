fe7536d9ba0c45f9b820d4d7ba526e711a6db3ca
shrutireddy1229 <43026623+shrutireddy1229@users.noreply.github.com>
2023-06-23 13:59:18 -0700

    Update streamlit_app.py
    Added the 1st breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy dinner - SF Badge2 project')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and blueberry meal')
+streamlit.text('Kale, Spinach and rocket smoothie')
+streamlit.text('Hard-Boiled free-range eggs')
