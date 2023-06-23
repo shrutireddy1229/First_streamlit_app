1d7d107e0a13766fe6e774c4ea162d00c409d79b
shrutireddy1229 <43026623+shrutireddy1229@users.noreply.github.com>
2023-06-23 14:22:19 -0700

    Update streamlit_app.py
    Display the table on the page using numbers

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,3 +16,6 @@ streamlit.dataframe(my_fruit_list)
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("pick some fruits:", list(my_fruit_list.index))
+
+#Display on list of items
+streamlit.dataframe(my_fruit_list)
